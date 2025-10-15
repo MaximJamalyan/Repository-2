5e8a7394013ac019713cc25f0c8711d175e75f5e
Максим Джамалян ип-511 <dzamalanmaksim@gmail.com>
2025-10-15 21:47:41 +0700

    Добавлен exercise1.c — среднее арифметическое

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+int main(void) {
+    double array[10];
+    double sum = 0;
+    printf("Введите десять вещественных чисел:\n");
+    for (int i = 0; i < 10; i++) {
+        scanf("%lf", &array[i]);
+        sum += array[i];
+    }
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+    printf("Среднее арифметическое: %.2lf\n", sum / 10.0);
     return 0;
 }
+
