3d4527dfa240b1eaaf3bd3e07a7d82e4342474fd
Максим Джамалян ип-511 <dzamalanmaksim@gmail.com>
2025-10-15 22:14:29 +0700

    Обновлён exercise4.c — вставкa элемента

--- src/exercise4.c
+++ src/exercise4.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        printf("Использование: ./exercise4 X K\n");
+        return 1;
+    }
+
+    double X;
+    int K;
+    sscanf(argv[1], "%lf", &X);
+    sscanf(argv[2], "%d", &K);
+
+    double array[10];
+    printf("Введите десять вещественных чисел:\n");
+    for (int i = 0; i < 10; i++) {
+        scanf("%lf", &array[i]);
+    }
+
+    for (int i = 9; i > K; i--) {
+        array[i] = array[i - 1];
+    }
+    array[K] = X;
+
+    printf("Новый массив:\n");
+    for (int i = 0; i < 10; i++) {
+        printf("%.2lf ", array[i]);
+    }
+    printf("\n");
+
     return 0;
 }
