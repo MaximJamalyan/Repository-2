3a7c21a6bc0540b2c2ebf865d88dd8b2047219e1
Максим Джамалян ип-511 <dzamalanmaksim@gmail.com>
2025-10-15 21:56:05 +0700

    Добавлен exercise2.c — инвертирование массива

--- src/exercise2.c
+++ src/exercise2.c
@@ -1,6 +1,19 @@
+
 #include <stdio.h>
 
-int main(int argc, char** argv) {
-    printf("Hello World!");
+int main(void) {
+    double array[10];
+
+    printf("Введите десять вещественных чисел:\n");
+    for (int i = 0; i < 10; i++) {
+        scanf("%lf", &array[i]);
+    }
+
+    printf("Инвертированный массив:\n");
+    for (int i = 9; i >= 0; i--) {
+        printf("%.2lf ", array[i]);
+    }
+
+    printf("\n");
     return 0;
 }
