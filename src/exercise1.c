a9b060b655ba3ee3597f55c6e56cf5d9630e3071
MaximJamalyan <dzamalanmaksim@gmail.com>
2025-10-17 09:24:47 +0700

    Update exercise1.c

--- src/exercise1.c
+++ src/exercise1.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-int main(void) {
+
+int main() {
     double array[10];
     double sum = 0;
-    printf("Введите десять вещественных чисел:\n");
-    for (int i = 0; i < 10; i++) {
+    printf("Введите 10 чисел : ");
+    for(int i = 0; i < 10; i++) {
         scanf("%lf", &array[i]);
         sum += array[i];
     }
-
-    printf("Среднее арифметическое: %.2lf\n", sum / 10.0);
+    
+    printf("%.2f\n", sum / 10);
     return 0;
-}
 
+}
