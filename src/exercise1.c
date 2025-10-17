326ea7ef8153a95f0ec381b8df4ccd6edd472b81
MaximJamalyan <dzamalanmaksim@gmail.com>
2025-10-17 11:45:02 +0700

    Update exercise1.c

--- src/exercise1.c
+++ src/exercise1.c
@@ -3,13 +3,12 @@
 int main() {
     double array[10];
     double sum = 0;
-    printf("Введите 10 чисел : ");
     for(int i = 0; i < 10; i++) {
         scanf("%lf", &array[i]);
         sum += array[i];
     }
     
-    printf("%.2f\n", sum / 10);
+    printf("%.3f\n", sum / 10);
     return 0;
 
 }
