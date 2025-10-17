23a67f0ebdd7ba3101ebc108f80a2a9f4377770d
MaximJamalyan <dzamalanmaksim@gmail.com>
2025-10-17 09:32:49 +0700

    Update exercise4.c

--- src/exercise4.c
+++ src/exercise4.c
@@ -1,32 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, char **argv) {
-    if (argc != 3) {
-        printf("Использование: ./exercise4 X K\n");
-        return 1;
-    }
-
-    double X;
-    int K;
-    sscanf(argv[1], "%lf", &X);
-    sscanf(argv[2], "%d", &K);
-
+int main(int argc, char** argv) {
+    double x = atof(argv[1]);
+    int k = atoi(argv[2]);
     double array[10];
-    printf("Введите десять вещественных чисел:\n");
-    for (int i = 0; i < 10; i++) {
+    
+    for(int i = 0; i < 10; i++) {
         scanf("%lf", &array[i]);
     }
-
-    for (int i = 9; i > K; i--) {
-        array[i] = array[i - 1];
+    
+    for(int i = 9; i > k; i--) {
+        array[i] = array[i-1];
     }
-    array[K] = X;
-
-    printf("Новый массив:\n");
-    for (int i = 0; i < 10; i++) {
-        printf("%.2lf ", array[i]);
+    array[k] = x;
+    
+    for(int i = 0; i < 10; i++) {
+        printf("%g ", array[i]);
     }
     printf("\n");
-
+    
     return 0;
 }
